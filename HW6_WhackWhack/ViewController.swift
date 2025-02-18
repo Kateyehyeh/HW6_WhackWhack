eaf6ce225c5fd7ceb0687f23dddd495391dffa4e
Kate Yeh <kateyehyeh@gmail.com>
2025-02-18 22:25:14 +0800

    Initial Commit

---
+++ HW6_WhackWhack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW6_WhackWhack
+//
+//  Created by Kateyeh on 2025/2/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
